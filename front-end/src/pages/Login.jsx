91d0bf1138b028c104c7a623b967a005aa6439a7
Alissa <90458438+AlissaMaximo@users.noreply.github.com>
2023-03-28 19:19:47 -0300

    Req 1 feito; não passa por causa do BE (#42)

---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Login() {
+  console.log('oi do login');
+  return (
+    <div>
+      <h4>Oi do Login</h4>
+    </div>
+  );
+}
+
+// Página para não dar erro 404 enquanto passa no R1
